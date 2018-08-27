e7e7b8718d09364a545d6cf10c4f93baf6e4990d
Assembly <Assembly@Creative.local>
2018-08-27 13:58:12 -0400

    updated first access.py

--- FirstAccess.py
+++ FirstAccess.py
@@ -14,7 +14,7 @@ access_token = '10205fa114f1778b74e3949320bcb275-b8fea4f40cc338ae9d44bd54f315484
 client = API(access_token=access_token)
 path = r'C:\Users\assembly\Desktop\PythonFolder\DataPipeline'
 
-url = 'https://api.powerbi.com/beta/8bf2e8dc-0738-49a0-9278-f0c7c067a1ca/datasets/b452c892-0ebf-47f0-b536-ddb9f4c25665/rows?key=1iyMaCWJ8247W2J6mxERUFy9707rXkRNOoKj0xC%2BNFTsrBFlX%2BiOz2jJ5PNN5GCFdm9nxjIno%2B19BXB%2By71iYA%3D%3D'
+url = 'https://api.powerbi.com/beta/8bf2e8dc-0738-49a0-9278-f0c7c067a1ca/datasets/93557faf-50d0-41d2-87d4-cfc1cb6de921/rows?key=%2FOMBGKDciezTIrWtQPplOSprqOebnuf6DtvIG4nAnPNOgs34NjIpcCL1CTO%2By6X5cin9i%2BSlltcEw9LVClFDOA%3D%3D'
 
 count = 0
 while(True):
@@ -31,7 +31,12 @@ while(True):
     data = '''[{{"item" :{0},"test 1" :{1},"test 2" :{2},"test 3" :{3},"test 4" :{4},"test 5" :{5},"test 6" :{6},"total" :{7}}}]'''.format(count,NAV[0],NAV[1],NAV[2],NAV[3],NAV[4],NAV[5],str(round(total,2)))
     response = requests.post(url, data=data)
     print(data,response)
-    time.sleep(0.8)
+    time.sleep(59.6)
 
-
+#for j in range(5):
+#    for i in range(1,1000):
+#        data = '''[{{"item" :{0},"test 1" :{1},"test 2" :{2},"test 3" :{3},"test 4" :{4},"test 5" :{5},"test 6" :{6},"total" :{7}}}]'''.format(i,0,0,0,0,0,0,0)
+#        response = requests.post(url, data=data)
+#        print(i*(j+1),response)
+#    time.sleep(1)
 
