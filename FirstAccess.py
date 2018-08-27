028cf909908a370bd7a28e68eb5cce1f76b367a1
Andrew <vandyand@gmail.com>
2018-08-27 15:39:30 -0400

    beta FirstAccess.py running on power bi

--- FirstAccess.py
+++ FirstAccess.py
@@ -15,7 +15,7 @@ access_token = '10205fa114f1778b74e3949320bcb275-b8fea4f40cc338ae9d44bd54f315484
 client = API(access_token=access_token)
 path = r'C:\Users\assembly\Desktop\PythonFolder\DataPipeline'
 
-url = 'https://api.powerbi.com/beta/8bf2e8dc-0738-49a0-9278-f0c7c067a1ca/datasets/0adcaad7-f379-4d60-b793-133425f0ec9a/rows?key=YKAYzibowzwDuVz0ECaQoaF9P5010YfgIuKMwsHuZcgmWmAPP1RK3RwD7bUZkvNa5e8dnVtLG5krHNKiXDODgw%3D%3D'
+url = 'https://api.powerbi.com/beta/8bf2e8dc-0738-49a0-9278-f0c7c067a1ca/datasets/b8330695-acd0-457e-995d-98ad2c785a53/rows?key=6y1%2Fl5RWhdJSy6s9Bx0WRI6JFdMRN13XaGhBZzVYCJqfle6dBLAVip%2BO1ND%2FFePg18Z0ptygEF3yEzl6L9CfVA%3D%3D'
 
 count = 0
 while(True):
@@ -30,11 +30,9 @@ while(True):
     total = sum(NAV)
     NAV = [ str(round(i,2)) for i in NAV ]
     current_time = str(datetime.now()).replace(' ','T')[:-3]+'Z'
-    data = '''[{{"datetime" :{0},"test 1" :{1},"test 2" :{2},"test 3" :{3},"test 4" :{4},"test 5" :{5},"test 6" :{6},"total" :{7}}}]'''.format(current_time,NAV[0],NAV[1],NAV[2],NAV[3],NAV[4],NAV[5],str(round(total,2)))
+    data = '''[{{"datetime" :"{0}","test 1" :{1},"test 2" :{2},"test 3" :{3},"test 4" :{4},"test 5" :{5},"test 6" :{6},"total" :{7}}}]'''.format(current_time,NAV[0],NAV[1],NAV[2],NAV[3],NAV[4],NAV[5],str(round(total,2)))
     response = requests.post(url, data=data)
     print(data,response)
     time.sleep(59.6)
 
-
-"2018-08-27T18:16:38.846Z"
 
